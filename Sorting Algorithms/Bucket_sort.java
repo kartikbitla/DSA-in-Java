bcb117ef69969ab5fd8a46b03811b8680951d8ae
Kartik Bitla <bitlakartik@gmail.com>
2023-10-27 14:22:38 +0000

    Bucket Sort

---
+++ Sorting Algorithms/Bucket_sort.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Bucket_sort {
+    int arr[];
+
+    public Bucket_sort(int arr[]){
+        this.arr = arr;
+    }
+
+
+    //printing whole array
+    public void printArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    //print buckets
+    public void printBuckets(ArrayList<Integer>[] buckets){
+        for(int i=0; i<buckets.length; i++){
+            System.out.println("\nBucket" + i + ":");
+
+            for(int j=0; j<buckets[i].size(); j++){
+                System.out.print(buckets[i].get(j) + " ");
+            }
+        }
+    }
+
+
+    //bucket sort
+    public void bucketSort(){
+        int numberOfBuckets = (int) Math.ceil(Math.sqrt(arr.length));
+        int maxValue = Integer.MIN_VALUE;
+
+        for(int value : arr){
+            if(value > maxValue){
+                maxValue = value;
+            }
+        }
+
+        
+    }
+}
