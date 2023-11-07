f0d31d3e6eda0aec2ac0888a79d1669c14b48587
Kartik Bitla <bitlakartik@gmail.com>
2023-11-07 14:09:22 +0000

    insert method in Trie

--- Trie/Main.java
+++ Trie/Main.java
@@ -1,8 +1,10 @@
-
-
 class Main{
     public static void main(String[] args){
         Trie newTrie = new Trie();
+        newTrie.insert("Kartik");
+        newTrie.insert("Kartiks");
+        newTrie.insert("KartikBitla");
+
         
     }
 }
