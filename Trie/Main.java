c590e88751d69ac3ef34f74e78aa7a6b3da7d025
Kartik Bitla <bitlakartik@gmail.com>
2023-11-07 13:49:39 +0000

    creation of Trie

---
+++ Trie/Main.java
@@ -0,0 +1,8 @@
+
+
+class Main{
+    public static void main(String[] args){
+        Trie newTrie = new Trie();
+        
+    }
+}
---
+++ Trie/Trie.java
@@ -0,0 +1,8 @@
+public class Trie{
+    private TrieNode root;
+
+    public Trie(){
+        root = new TrieNode();
+        System.out.println("The Trie has been created");
+    }
+}
