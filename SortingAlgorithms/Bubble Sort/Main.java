03d033e416f8cecaacb1cf2dbedec839c932fef5
Kartik Bitla <bitlakartik@gmail.com>
2023-11-27 15:17:51 +0000

    Buuble Sort

---
+++ SortingAlgorithms/Bubble Sort/Main.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void main(String[] args){
+        BubbleSort bs = new BubbleSort();
+        int arr[] = {10, 5, 30, 15, 50, 6};
+        bs.bubbleSort(arr);
+        bs.printArray(arr);
+    }
+}
