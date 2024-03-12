b0e9cdadf5edcfd924f11f9c94ec7fea6f354c96
Kartik Bitla <bitlakartik@gmail.com>
2024-03-12 20:20:51 +0530

    Update Main.java

--- SortingAlgorithms/Main.java
+++ SortingAlgorithms/Main.java
@@ -5,8 +5,7 @@ class Main{
         //sort.bubbleSort(arr);
         //sort.selectionSort(arr);
         //sort.insertionSort(arr);
-        //sort.printArray(arr);
-        
+        //sort.printArray(arr);       
 
         int arr[] = {9,7,5,4,2,1,3,6,8};
         Bucket_sort bucket_sort = new Bucket_sort(arr);
