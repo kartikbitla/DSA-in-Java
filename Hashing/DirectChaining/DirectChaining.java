33b98c8b1c8d4e652b3fb3f513c0c1bc06cb2f20
Kartik Bitla <bitlakartik@gmail.com>
2024-03-17 06:04:25 +0530

    Update DirectChaining.java

--- Hashing/DirectChaining/DirectChaining.java
+++ Hashing/DirectChaining/DirectChaining.java
@@ -51,7 +51,6 @@ public class DirectChaining {
     }
 
 
-
     //search en element in the Hash Table and return index and boolean value
     public boolean searchHashTable(String word){
         int index = modeASCIIFunction(word, hashTable.length);
