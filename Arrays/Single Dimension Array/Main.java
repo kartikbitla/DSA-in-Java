b28a1b8e9f002f153a840f50a67216e16bf17534
Kartik Bitla <bitlakartik@gmail.com>
2024-03-27 12:24:51 +0530

    Update Main.java

--- Arrays/Single Dimension Array/Main.java
+++ Arrays/Single Dimension Array/Main.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 class Main{
-    public static void main(String[] agrs){
-        
+    public static void main(String[] agrs){        
         SingleDimensionArray sda = new SingleDimensionArray(10);
         sda.insert(0,0);
         sda.insert(1,10);
